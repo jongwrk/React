692ca8d1c1a5ab4f01b29c1d7037fa03fd581a28
jwhan-dix <247330828+jongwrk@users.noreply.github.com>
2026-04-14 22:51:34 +0900

    [update] finish
    - useEffect로 최초 렌더링 이후 api 호출
    - 주시하는 대상이 없으므로 최초 마운트 될 때만 호출
    - api 호출이 종료될 때 데이터 파싱 및 로딩 표시 삭제

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,7 +1,72 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
 function App() {
+  const listStyle = "card-list";
+  const cardStyle = "card-item";
+
+  const [loading, setLoading] = useState(true);
+  const [coinList, setCoinList] = useState([]);
+
+  async function fetchData() {
+    if (!loading) {
+      setLoading(true);
+    }
+
+    const response = await fetch(
+      "https://api.coinpaprika.com/v1/tickers?limit=100&quotes=KRW",
+    );
+
+    if (!response.ok) {
+      console.error("ERROR");
+      return;
+    }
+
+    const data = await response.json();
+    // console.log(data);
+    setCoinList(data);
+    setLoading(false);
+
+    // setTimeout(() => {
+    //   const data = [
+    //     {
+    //       index: 1,
+    //       name: "ABC",
+    //       quotes: {
+    //         KRW: {
+    //           price: 123123123,
+    //         },
+    //       },
+    //     },
+    //   ];
+    //   setList(data);
+    //   setLoading(false);
+    // }, 3000);
+  }
+
+  useEffect(() => {
+    fetchData();
+    // eslint-disable-next-line
+  }, []);
+
   return (
     <div>
-      <p>To do list!</p>
+      <h1>The Coin Lists!</h1>
+      {loading ? (
+        "loading,,,"
+      ) : (
+        <div>
+          <h4>counts: {coinList.length}</h4>
+          <ul className={listStyle}>
+            {coinList.map((item, index) => (
+              <li className={cardStyle} key={index}>
+                {item.name}({item.symbol}){" "}
+                {item.quotes.KRW.price} KRW
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
