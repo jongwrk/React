7c64cf8818e78f95ac9f25155da53d9a90de4329
jwhan-dix <247330828+jongwrk@users.noreply.github.com>
2026-04-14 18:16:31 +0900

    [update] finish
    - 상태를 가진 배열 활용
    - map 함수 콜백으로 원소 나열

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,7 +1,51 @@
+import { useState } from "react";
+
 function App() {
+  const [todo, setTodo] = useState("");
+  const [todoList, setTodoList] = useState([]);
+
+  function onChange(event) {
+    const newValue = event.target.value;
+
+    setTodo(newValue);
+  }
+
+  function onSubmit(event) {
+    event.preventDefault();
+
+    if (todo === "") {
+      alert("Nothing was entered!");
+      return;
+    }
+
+    setTodoList((current) => [...current, todo]);
+    setTodo("");
+  }
+
+  function onClear() {
+    setTodo("");
+    setTodoList([]);
+  }
+
   return (
     <div>
-      <p>To do list!</p>
+      <h1>My TODO List! ({todoList.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          placeholder="Input your jobs,,,"
+          onChange={onChange}
+          type="text"
+          value={todo}
+        ></input>
+        <button>Add</button>
+      </form>
+      <button onClick={onClear}>Clear</button>
+      <hr />
+      <ul>
+        {todoList.map((value, index) => (
+          <li key={index}>{value.toUpperCase()}</li>
+        ))}
+      </ul>
     </div>
   );
 }
