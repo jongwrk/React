4f8bdad04d45b748e9e979b1a8bee7295627d5bb
jwhan-dix <247330828+jongwrk@users.noreply.github.com>
2026-04-15 15:17:41 +0900

    [update] fetch, pagination
    - useState는 비동기이므로 페이지 변경 후 직접 호출하면 동작하지 않는 오류
    - useEffect로 page를 주시하고, 변경될 때 fetch되도록 적용
    - 최초 렌더링 때에도 참조 평가를 하므로 분리하지 않아도 똑같이 실행됨

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,7 +1,108 @@
+import { useEffect, useState } from "react";
+//
+import Loading from "./Loading";
+//
+import "./App.css";
+
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [movieList, setMovieList] = useState([]);
+  const [movieCount, setMovieCount] = useState(0);
+  const [page, setPage] = useState(1);
+  const [lastPage, setLastPage] = useState(1);
+  //
+  const LIMIT = 20;
+  const ORDERBY = "desc";
+  const SORTBY = "rating";
+  const MINRATING = 8.5;
+
+  async function fetchData() {
+    if (!isLoading) {
+      setIsLoading(true);
+      setMovieList([]);
+    }
+
+    const param = new URLSearchParams({
+      limit: LIMIT,
+      order_by: ORDERBY,
+      sort_by: SORTBY,
+      minimum_rating: MINRATING,
+      page: page,
+    });
+
+    const API_URL = `https://movies-api.accel.li/api/v2/list_movies.json?${param}`;
+    const response = await fetch(API_URL);
+
+    if (!response.ok) {
+      alert("The Service is in error!\nPlease try again.");
+      return;
+    }
+
+    const data = await response.json();
+
+    setMovieCount(data.data.movie_count);
+    setMovieList(data.data.movies);
+    setLastPage(Math.round(data.data.movie_count / LIMIT));
+    setIsLoading(false);
+  }
+
+  function clickPrev() {
+    if (page === 1) {
+      alert("No more pages.");
+      return;
+    }
+
+    setPage((current) => current - 1);
+  }
+
+  function clicNext() {
+    if (page === lastPage) {
+      alert("No more pages.");
+      return;
+    }
+
+    setPage((current) => current + 1);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, [page]);
+
   return (
     <div>
-      <p>To do list!</p>
+      <h1>Hello Moive World!</h1>
+
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <div>
+          <h4>total search counts: {movieCount}</h4>
+          <p>
+            <button onClick={clickPrev}>prev</button>
+            {page} / {lastPage}
+            <button onClick={clicNext}>next</button>
+          </p>
+          <div className="container">
+            {movieList.map((item) => (
+              <div key={item.id} className="card">
+                <img src={item.medium_cover_image} />
+                <div>
+                  <h2>
+                    {item.title_long} | ({item.rating} /{" "}
+                    {item.runtime}min)
+                  </h2>
+                  <ul>
+                    {item.genres.map((genre, index) => (
+                      <li key={index}>{genre}</li>
+                    ))}
+                  </ul>
+                  <p>{item.summary}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
