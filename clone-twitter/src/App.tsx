92e95388a7e50f2fdcf27b6b49b80f04bfbbadb0
jonwrk <247330828+jongwrk@users.noreply.github.com>
2026-04-22 15:58:29 +0900

    [add] set route
    - 기본 라우트 설정
    - Outlet은 "중첩라우팅의 렌더링 슬롯", 즉 자식 라우트가 들어갈 자리임

--- clone-twitter/src/App.tsx
+++ clone-twitter/src/App.tsx
@@ -1,7 +1,45 @@
-import "./App.css";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Layout from "./component/layout";
+import Home from "./route/home";
+import Profile from "./route/profile";
+import Login from "./route/login";
+import SignUp from "./route/signup";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "profile",
+        element: <Profile />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/signup",
+    element: <SignUp />,
+  },
+]);
 
 function App() {
-  return <div>Hello world!</div>;
+  return (
+    <div>
+      <div>Hello world!</div>
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
